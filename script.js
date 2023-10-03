d0b41bb86a90b60bd31e4282af17aa4d55795c49
mariatexdorf07 <125214160+mariatexdorf07@users.noreply.github.com>
2023-10-03 10:42:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,50 @@ function funcaoResposta(){
     if(respostaUsuario){
         if(respostaUsuario.value === respostaCorreta){
             document.getElementById("retorno").textContent = "Correto!";
+            
         }
         else{
             document.getElementById("retorno").textContent = "Errado!";
+           
         }
     }
     else{
         document.getElementById("retorno").textContent = "Tem que escolher alguma!!";
     }    
 }
-prompt maria (escreva aqui)
+
+function funcaoResposta2(){
+    var respostaCorreta = "correta";
+    var respostaUsuario = document.querySelector('input[name="resposta"]:checked');
+    if(respostaUsuario){
+        if(respostaUsuario.value === respostaCorreta){
+            document.getElementById("maria").textContent = "Correto!";
+            
+        }
+        else{
+            document.getElementById("maria").textContent = "Errado!";
+           
+        }
+    }
+    else{
+        document.getElementById("maria").textContent = "Tem que escolher alguma!!";
+    }    
+}
+
+function funcaoResposta3(){
+    var respostaCorreta = "correta";
+    var respostaUsuario = document.querySelector('input[name="resposta"]:checked');
+    if(respostaUsuario){
+        if(respostaUsuario.value === respostaCorreta){
+            document.getElementById("jesus").textContent = "Correto!";
+            
+        }
+        else{
+            document.getElementById("jesus").textContent = "Errado!";
+           
+        }
+    }
+    else{
+        document.getElementById("jesus").textContent = "Tem que escolher alguma!!";
+    }    
+}
